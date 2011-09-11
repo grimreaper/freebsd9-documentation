72d7277ff03e58caf7787d365f62363b5ebc5961
glen.j.barber <glen.j.barber@f90f8275-59fd-90e3-2457-e9098a0e1804>
2011-09-11 21:26:23 +0000

    Copy doc tree from trunk/

--- www/es/news/clickable.pl
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/perl -np
-# Copyright (c) Juli 1998 Wolfram Schneider <wosch@FreeBSD.org>. Berlin.
-#
-# clickable - Make URL clickable
-#
-# $FreeBSD: www/es/news/clickable.pl,v 1.2 1999/09/06 07:03:03 peter Exp $
-
-s/</&lt;/g;
-s%((http|ftp)://[^\s"\)\>,;]+)%<A HREF="$1">$1</A>%gi;
